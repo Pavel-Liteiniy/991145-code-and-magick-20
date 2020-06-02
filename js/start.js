c1e45d53e74965b10f7c232d62765c51c8420ccc
Pavel <paulsev96@gmail.com>
2020-06-02 22:59:02 +0300

    Универсализирует функцию добавления текста

--- js/start.js
+++ js/start.js
@@ -10,21 +10,24 @@ var FONT_GAP = 15;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var BAR_HEIGHT = 150;
+
 var titleRows = ['Ура вы победили!', 'Список результатов:'];
 
-var renderTitle = function (context, rows) {
-  context.font = '16px PT Mono';
-  context.textBaseline = 'hanging';
+var drawText = function (ctx, text, x, y, color, font) {
+  ctx.font = font || '16px PT Mono';
+  ctx.fillStyle = color || '#000000';
+  ctx.fillText(text, x, y);
+};
 
+var renderTitle = function (context, rows) {
   for (var i = 0; i < rows.length; i++) {
-    context.fillText(rows[i], CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2 + FONT_GAP * i);
+    drawText(context, rows[i], CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3 + FONT_GAP * i);
   }
 };
 
-var addBarDescription = function (ctx, color, playerName, playerTime, barHeight, startPoint) {
-  ctx.fillStyle = color;
-  ctx.fillText(playerName, startPoint, CLOUD_BOTTOM - GAP * 2);
-  ctx.fillText(Math.round(playerTime), startPoint, CLOUD_BOTTOM - GAP * 3 - FONT_GAP - barHeight);
+var addBarDescription = function (ctx, playerName, playerTime, barHeight, startPoint) {
+  drawText(ctx, playerName, startPoint, CLOUD_BOTTOM - GAP * 2);
+  drawText(ctx, Math.round(playerTime), startPoint, CLOUD_BOTTOM - GAP * 4 - FONT_GAP - barHeight);
 };
 
 var renderCloud = function (ctx, x, y, width, height, color) {
@@ -57,20 +60,15 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#ffffff');
 
-  ctx.fillStyle = '#000000';
-
   renderTitle(ctx, titleRows);
 
   var maxTime = getMaxElement(times);
-
-  ctx.textAlign = 'left';
-  ctx.textBaseline = 'bottom';
 
   for (var i = 0; i < players.length; i++) {
     var barItemHeight = (BAR_HEIGHT * times[i]) / maxTime;
     var barX = CLOUD_X + GAP * 3 + BAR_GAP * 2 * i;
 
-    addBarDescription(ctx, '#000000', players[i], times[i], barItemHeight, barX);
+    addBarDescription(ctx, players[i], times[i], barItemHeight, barX, '#000000');
 
     renderCloud(ctx, barX, CLOUD_BOTTOM - GAP * 3 - FONT_GAP - barItemHeight, BAR_WIDTH, barItemHeight, players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : getRandomBlueSaturate());
   }
